b318722f58c626d75fa8f49e49f947c1f9a2da69
Marcelo Carvalho <marcelo.scientist@hotmail.com>
2022-07-21 17:58:22 -0300

    Restarting project and adding up the first api consuming

---
+++ src/Pages/contato.js
@@ -0,0 +1,39 @@
+import { Grid, Button, TextField } from '@material-ui/core/';
+import { useState, useEffect } from 'react';
+
+const Contatos = () => {
+    
+    const [message, setMessage] = useState([])
+    useEffect(async()=>{
+        const response = await fetch('http://localhost:5000/message')
+        const data = await response.json()
+        setMessage(data);
+
+    },[])
+
+    console.log(message)
+
+    return(
+        <>
+            <Grid container direction="row" xs={12}>
+                <TextField id="name" label="Name" fullWidth/>
+                <TextField id="message" label="Message" fullWidth/>
+            </Grid>
+
+            <Button className="mt-2" variant="contained" color="primary">
+                Sent
+            </Button>
+            <div className="card mt-2">
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text">
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Contatos;
